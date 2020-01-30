31b0897c9d8d7cd035111ae99043ff22590bcbb5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-30 18:33:38 +0100

    upload

--- countdown.py
+++ countdown.py
@@ -5,7 +5,9 @@ from PyQt5 import QtWidgets, uic, QtCore
 
 class Ui(QtWidgets.QMainWindow):
     VOKALE = ["A", "E", "I", "O", "U"]
-    unused_text_labels = 1
+    KONSONANTEN = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y",
+                   "Z"]
+    unused_text_labels = 0
 
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
@@ -13,28 +15,41 @@ class Ui(QtWidgets.QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.resize(300, 200)
-
         self.vokal = self.findChild(QtWidgets.QPushButton, "vokal")  # Find Vokal Button
-        self.vokal.clicked.connect(self.vokal_generator)
+        self.vokal.clicked.connect(self.vokal_generator)  # call function
 
         self.konsonant = self.findChild(QtWidgets.QPushButton, "konsonant")  # Find Konsonant Button
+        self.konsonant.clicked.connect(self.konsonanten_generator)  # call function
 
         self.exit = self.findChild(QtWidgets.QPushButton, "beenden")  # Find beenden Button
-        self.exit.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.exit.clicked.connect(QtCore.QCoreApplication.instance().quit)  # quit
+
+        self.error_msg = self.findChild(QtWidgets.QLabel, "error_msg")
+
         self.labels = []
-        for i in range(1, 10):
+        for i in range(1, 10):  # find all labels
             name = "buchstabe_{}".format(i)
             label = self.findChild(QtWidgets.QLabel, name)
             self.labels.append(label)
-
         self.show()  # Show the GUI1
 
     def vokal_generator(self):
-        new_vokal = Ui.VOKALE[randint(0, 4)]
-        self.labels[Ui.unused_text_labels].setText(new_vokal)
-        Ui.unused_text_labels += 1
+        if Ui.unused_text_labels < 9:
+            new_vokal = Ui.VOKALE[randint(0, 4)]
+            self.labels[Ui.unused_text_labels].setText(new_vokal)
+            Ui.unused_text_labels += 1
+        else:
+            self.error_msg.setText("Du hast schon 9 Buchstaben!")
         print("Vokal pressed")
+
+    def konsonanten_generator(self):
+        if Ui.unused_text_labels < 9:
+            new_konsonant = Ui.KONSONANTEN[randint(0, 21)]
+            self.labels[Ui.unused_text_labels].setText(new_konsonant)
+            Ui.unused_text_labels += 1
+        else:
+            self.error_msg.setText("Du hast schon 9 Buchstaben!")
+        print("Konsonant pressed")
 
 
 app = QtWidgets.QApplication(sys.argv)
